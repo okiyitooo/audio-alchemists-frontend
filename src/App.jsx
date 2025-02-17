bec2dcff01aa84e6f259984b429121ccf72268a3
okiyitooo <kanaetochi.okiyi@udc.edu>
2025-02-17 17:30:44 -0500

    Wrap application in ThemeProvider for MUI theme integration

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,32 @@ import SignupPage from './pages/SignupPage';
 import Dashboard from './components/Dashboard';
 import ProjectPage from './pages/ProjectPage';
 import HomePage from './pages/HomePage';
+import { ThemeProvider, createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#ff6f61',
+    },
+    secondary: {
+      main: '#ffcc5c',
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/projects/:id" element={<ProjectPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects/:id" element={<ProjectPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
